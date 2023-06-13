c728157b3127016ce6d0148578c572b2fe2c7de7
Aidan Peters <avpeters2004@gmail.com>
2023-06-13 16:39:18 -0400

    Started moving layout around to get alternate on the tab still looks messy but everything there and functioning

---
+++ clearConfig.py
@@ -0,0 +1,11 @@
+
+import yaml
+
+
+info= {"folderPath": "","DLCfolderPath": "","videoPaths":[],"DLCvideoPaths":[], "points":[],"BoxHeight":"","BoxWidth":"","Name":"","Path":"","numFeatures":"30",
+                "data":
+                {' Videos':[],
+                    'Feature 1': [],
+                    'Feature 2': []}}
+with open('config.yaml', 'w') as file:
+    yaml.dump(info,file,default_flow_style=False) 
