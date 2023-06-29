eea0181ff684bdd4f412cc021ca4aaab25ac5415
Aidan Peters <avpeters2004@gmail.com>
2023-06-29 14:42:14 -0400

    Fixed most video bugs (add to documenation that if slider gets messed up just hit stop button then should work fine and normal click slider once, video should pause and then move slider) also worked on pop ups but new ones aren't really nessisary and added the uncertainty cut off but have yet to incorporate it

--- clearConfig.py
+++ clearConfig.py
@@ -1,11 +1,21 @@
 
 import yaml
-
+class NoAliasDumper(yaml.SafeDumper):
+    """Prevents yaml from using id to save space (would potentially mess up certain functions)"""
+    def ignore_aliases(self, data):
+        return True
 
 info= {"folderPath": "","DLCfolderPath": "","videoPaths":[],"DLCvideoPaths":[], "zone":[],"BoxHeight":"","BoxWidth":"","Name":"","Path":"","numFeatures":"2",
                 "data":
                 {' Videos':[],
                     'Feature 1': [],
-                    'Feature 2': []}}
+                    'Feature 2': []},
+                    "uncertainty":0.6}
 with open('config.yaml', 'w') as file:
     yaml.dump(info,file,default_flow_style=False) 
+
+
+helper= {"ValidationVideoSettings":{"PlayBackSpeed":1,"Frame":0,"FPS":30,"TotalFrames":1},"PartsOfInterest":[]}
+print(len(helper))
+with open('helper.yaml', 'w') as file:
+    yaml.dump(helper,file,default_flow_style=False, Dumper=NoAliasDumper) 
