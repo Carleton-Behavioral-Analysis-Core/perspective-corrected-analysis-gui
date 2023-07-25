f77846770531994599cfbb8ff515b22dd8445b2e
Aidan Peters <avpeters2004@gmail.com>
2023-07-25 14:30:14 -0400

    3 of 4

--- clearConfig.py
+++ clearConfig.py
@@ -4,20 +4,20 @@ class NoAliasDumper(yaml.SafeDumper):
     """Prevents yaml from using id to save space (would potentially mess up certain functions)"""
     def ignore_aliases(self, data):
         return True
-
+# centroid = transformed_df.groupby(level="coords", axis=1).mean()
 info = {
                 "folderPath": "",
                 "DLCfolderPath": "",
                 "videoPaths": [],
                 "DLCvideoPaths": [],
-                "zones": {"center": []},
+                "analysis_zones": {"center": []},
                 "BoxHeight": 500,
                 "BoxWidth": 500,
                 "Name": "",
                 "Path": "",
                 "data": {" Videos": [], "Feature 1": [], "Feature 2": []},
-                "uncertainty": 0.6,
-                "groups": [],
+                "dlc_pcutoff": 0.6,
+                "treatment_groups": [],
                 "threshold": 20
             }
 with open('config.yaml', 'w') as file:
@@ -25,15 +25,15 @@ with open('config.yaml', 'w') as file:
 
 
 helper = {
-            "ValidationVideoSettings": {
-                "PlayBackSpeed": 1,
-                "Frame": 0,
-                "FPS": 30,
-                "TotalFrames": 1,
-            },
-            "PartsOfInterest": [],
-            "points": [],
-            "Zones":{}
-        }
+                "ValidationVideoSettings": {
+                    "PlayBackSpeed": 1,
+                    "Frame": 0,
+                    "FPS": 30,
+                    "TotalFrames": 1,
+                },
+                "PartsOfInterest": [],
+                "registration_points": [],
+                "Zones":{}
+            }
 with open('helper.yaml', 'w') as file:
     yaml.dump(helper,file,default_flow_style=False, Dumper=NoAliasDumper) 
