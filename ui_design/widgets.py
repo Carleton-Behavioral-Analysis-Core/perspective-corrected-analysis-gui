d5e96620dc2eddc91009504caafd797386666445
Aidan Peters <avpeters2004@gmail.com>
2023-07-27 13:17:14 -0400

    Made changes to the register widget and now it will display the colors where clicked (it uses a dummy list that will be removed in real versions just trying to verify that it works)

--- ui_design/widgets.py
+++ ui_design/widgets.py
@@ -1,9 +1,10 @@
 import numpy as np
 import os
 from PyQt6 import QtCore
-from PyQt6.QtGui import QImage, QPixmap
+from PyQt6.QtGui import QImage, QPixmap, QPen, QColor
 from PyQt6.QtWidgets import QFileDialog, QHBoxLayout, QVBoxLayout, QWidget, QLabel, QLineEdit, QPushButton, QSizePolicy, QComboBox
-
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QPainter
 class EmptyQVBoxLayout(QVBoxLayout):
     def __init__(self):
         super().__init__()
@@ -122,8 +123,14 @@ class MaterialLabelImageRegisterPoints(QWidget):
         self.height = height
         self.width = width
         placeholder_frame = np.zeros(shape=(self.height,self.width,3), dtype=np.uint8)
+        self.testHolder = [] #delete when real
         self.set_image(placeholder_frame)
+
+        # Test delete when real
+        
+            # Test delete when real
         if Clickable:
+            
             """ insert config file refernce like config = openYaml()
             self.points_storage = {}
             {for i in range(len(config["video_data"])): /or for key in config["video_data"]:}
@@ -147,11 +154,63 @@ class MaterialLabelImageRegisterPoints(QWidget):
         if (h != self.height) or (w != self.width):
             self.height = h
             self.width = w
+        
+        
             
         bytes_per_line = ch * w
         image = QImage(image.data, w, h, bytes_per_line, QImage.Format.Format_RGB888)
         image = image.scaled(self.width, self.height, QtCore.Qt.AspectRatioMode.KeepAspectRatio)
         pixmap = QPixmap(image)
+        # Test delete when real
+        # try:
+        if True:
+            for j in range(len(self.testHolder)):
+                # self.points_storage[self.video_combo_box.combo_box.currentText()]
+                painter = QPainter(pixmap)
+                if j == 0:
+                    pen = QPen(QColor('red'))
+                elif j == 1:
+                    pen = QPen(QColor('magenta'))
+                elif j == 2:
+                    pen = QPen(QColor('cyan'))
+                elif j == 3:
+                    pen = QPen(QColor('green'))
+
+                pen.setWidth(10)
+
+                painter.setPen(pen)
+                painter.drawPoint(
+                    int(self.testHolder[j][0]),
+                    int(self.testHolder[j][1])
+                )
+                painter.end()
+        # except:
+        #     pass
+        # Test delete when real
+
+        # try:
+        #     for j in range(len(self.points_storage[self.video_combo_box.combo_box.currentText()])):
+        #         self.points_storage[self.video_combo_box.combo_box.currentText()]
+        #         painter = QPainter(pixmap)
+        #         if j == 0:
+        #             pen = QPen(Qt.red)
+        #         elif j == 1:
+        #             pen = QPen(Qt.magenta)
+        #         elif j == 2:
+        #             pen = QPen(Qt.cyan)
+        #         elif j == 3:
+        #             pen = QPen(Qt.green)
+
+        #         pen.setWidth(10)
+
+        #         painter.setPen(pen)
+        #         painter.drawPoint(
+        #             int(self.points_storage[self.video_combo_box.combo_box.currentText()][j][0]),
+        #             int(self.points_storage[self.video_combo_box.combo_box.currentText()][j][1])
+        #         )
+        #         painter.end()
+        # except:
+        #     pass
         self.frame_label.setPixmap(pixmap)
 
     def locationInImageCorner(self, event):
@@ -179,10 +238,25 @@ class MaterialLabelImageRegisterPoints(QWidget):
                     / self.frame_label.height() #divid
                 )
                 print(cord)
+                # Test delete when real
+                print(self.testHolder)
+                # self.testHolder
+                if len(self.testHolder) < 4:
+                    self.testHolder.append(cord)
+                else:
+                  self.testHolder[3] = cord
+
+                placeholder_frame = np.zeros(shape=(self.height,self.width,3), dtype=np.uint8)
+                self.set_image(placeholder_frame)
+                # Test delete when real
+
                 #if len(self.points_storage[self.video_combo_box.combo_box.currentText()]) < 4:
                     # self.points_storage[self.video_combo_box.combo_box.currentText()].append(cord)
                 # else:
                 #   self.points_storage[self.video_combo_box.combo_box.currentText()][3] = cord
+
+
+
              
             
         # except:
